3220c8cb61667c7f2f4b61dddc645ffc371b0239
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-12-24 21:53:55 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,4 @@ RELEASE="$(rpm -E %fedora)"
 # Set up first-setup system
 cp -r /ctx/system/firstsetup /etc/
 cp /ctx/services/systemsetup.service /etc/systemd/system
-cp /ctx/services/fix.service /etc/systemd/system
 systemctl enable systemsetup.service
-systemctl enable fix.service
