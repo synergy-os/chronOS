d5bc5f10ddda26a5f7c74d36831e3b658ccec461
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-12-24 19:54:16 -0500

    fix: do not setup two-auth during build

--- build.sh
+++ build.sh
@@ -9,10 +9,6 @@ RELEASE="$(rpm -E %fedora)"
 /ctx/scripts/packages.sh
 /ctx/scripts/quirks.sh
 
-# Set up two-auth system
-useradd -M --system -s /bin/false Admin
-echo Admin:default | chpasswd
-
 # Set up first-setup system
 cp -r /ctx/system/firstsetup /etc/
 mkdir /etc/skel/.config/
