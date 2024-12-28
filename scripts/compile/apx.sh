ef0209b9e10b9517ef0f196ccdc4a65386d07311
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-12-28 01:02:54 -0800

    Update apx.sh

--- scripts/compile/apx.sh
+++ scripts/compile/apx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ouex pipefail
-rpm-ostree install go git make podman meson glib2-devel
+rpm-ostree install go git make podman meson glib2-devel desktop-file-utils
 
 # apx
 git clone --recursive https://github.com/Vanilla-OS/apx.git
