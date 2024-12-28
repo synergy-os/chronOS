14b8461e9b1abbd0e86990ed7271c232f329b410
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-12-28 01:51:32 -0800

    Update apx.sh

--- scripts/compile/apx.sh
+++ scripts/compile/apx.sh
@@ -3,7 +3,6 @@
 set -ouex pipefail
 rpm-ostree install go git make podman meson glib2-devel desktop-file-utils
 
-# apx
 git clone --recursive https://github.com/Vanilla-OS/apx.git
 cd apx
 make build
@@ -11,12 +10,3 @@ make install
 make install-manpages
 cd ..
 rm -rf apx
-
-# apx-gui
-git clone https://github.com/Vanilla-OS/apx-gui
-cd apx-gui
-meson setup build
-ninja -C build
-ninja -C build install
-cd ..
-rm -rf apx-gui
