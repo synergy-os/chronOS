4ea5f17cf88a3d27d392034a25f28dac4b0a96fa
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2024-12-27 21:27:48 -0800

    idfk

--- scripts/compile/apx.sh
+++ scripts/compile/apx.sh
@@ -2,4 +2,22 @@
 
 set -ouex pipefail
 rpm-ostree install go git make podman
-cat /root
+
+# apx
+cd ~
+git clone --recursive https://github.com/Vanilla-OS/apx.git
+cd apx
+make build
+make install
+make install-manpages
+cd ~
+rm -rf apx
+
+# apx-gui
+git clone https://github.com/Vanilla-OS/apx-gui
+cd apx-gui
+meson setup build
+ninja -C build
+ninja -C build install
+cd ~
+rm -rf apx-gui
