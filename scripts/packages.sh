a4b14a6b147eac92add1c537d589cc3d0c973074
certifiedfoolio <mastersvanity@gmail.com>
2024-12-31 13:29:49 -0800

    feat: change terminal to Ptyxis

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,14 +4,6 @@ set -ouex pipefail
 
 # install pkgs
 rpm-ostree install gstreamer1-plugin-openh264 \
-	blackbox-terminal \
-	gettext \
-	python3-gobject \
-	python3-pyyaml \
-	python3-requests \
-	gobject-introspection \
-	gobject-introspection-devel \
- 	util-linux \
 	clapper \
 	flatseal \
 	secrets \
@@ -21,6 +13,5 @@ rpm-ostree install gstreamer1-plugin-openh264 \
 # remove pkgs from base image
 rpm-ostree override remove htop \
 	nvtop \
-	ptyxis \
 	gnome-system-monitor \
 	toolbox
