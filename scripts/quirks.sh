e430d1dd674fb697232aec5644b1c8e87882f5c5
certifiedfoolio <mastersvanity@gmail.com>
2024-12-31 05:22:49 -0800

    chore: QOL fix

--- scripts/quirks.sh
+++ scripts/quirks.sh
@@ -12,17 +12,10 @@ rm /usr/share/apx/stacks/vanilla.yaml \
     /usr/share/apx/stacks/vanilla-dev.yaml \
     /usr/share/apx/stacks/opensuse-leap-15.6.yaml
 
-# move nix path
-if [ -d "/nix" ];
-then
-    mv /nix /var/nix-store
-else
-    mkdir /var/nix-store
-fi
-
 # copy system files
 cp -r /ctx/system/firstsetup /etc/
 cp -r /ctx/services/* /etc/systemd/system
+mv /nix /var/nix-store
 
 # enable systemd services/mounts
 systemctl enable nix-daemon # enable nix daemon
