40fb75ebf63a457d25f5e1e6daaeec4d35d04b89
certifiedfoolio <mastersvanity@gmail.com>
2024-12-31 12:08:15 -0800

    feat: use up-to-date nix copr

--- scripts/reposetup.sh
+++ scripts/reposetup.sh
@@ -5,5 +5,5 @@ set -ouex pipefail
 wget -O /etc/yum.repos.d/_copr_pvermeer-chromebook-linux-audio.repo \
     https://copr.fedorainfracloud.org/coprs/pvermeer/chromebook-linux-audio/repo/fedora-41/pvermeer-chromebook-linux-audio-fedora-41.repo
 wget -O /etc/yum.repos.d/petersen-nix-fedora-41.repo \
-    https://copr.fedorainfracloud.org/coprs/petersen/nix/repo/fedora-41/petersen-nix-fedora-41.repo
+    https://copr.fedorainfracloud.org/coprs/poscat/Nix/repo/fedora-41/poscat-Nix-fedora-41.repo
 cp /ctx/scripts/localrepos/google-chrome.repo /etc/yum.repos.d
