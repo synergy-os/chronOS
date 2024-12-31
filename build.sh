20fe634e6e392882bfbe913a2424573c48b7fd27
certifiedfoolio <mastersvanity@gmail.com>
2024-12-31 05:08:19 -0800

    feat: bind mount to /var/nix-store

--- build.sh
+++ build.sh
@@ -8,8 +8,3 @@ RELEASE="$(rpm -E %fedora)"
 /ctx/scripts/reposetup.sh
 /ctx/scripts/packages.sh
 /ctx/scripts/quirks.sh
-
-# Set up first-setup system
-cp -r /ctx/system/firstsetup /etc/
-cp /ctx/services/systemsetup.service /etc/systemd/system
-systemctl enable systemsetup.service
--- scripts/quirks.sh
+++ scripts/quirks.sh
@@ -5,8 +5,26 @@ set -ouex pipefail
 # fixes
 ln -s /usr/bin/rpm-ostree /usr/bin/chruln
 ln -s /usr/bin/systemctl /usr/bin/cserv
+
+# systemsetup and apx
 cp /ctx/system/applications/sysupdate.desktop /usr/share/applications/
 rm /usr/share/apx/stacks/vanilla.yaml \
     /usr/share/apx/stacks/vanilla-dev.yaml \
     /usr/share/apx/stacks/opensuse-leap-15.6.yaml
-systemctl enable nix-daemon
+
+# move nix path
+if [ -d "/nix" ];
+then
+    mv /nix /var/nix-store
+else
+    mkdir /var/nix-store
+fi
+
+# copy system files
+cp -r /ctx/system/firstsetup /etc/
+cp -r /ctx/services/* /etc/systemd/system
+
+# enable systemd services/mounts
+systemctl enable nix-daemon # enable nix daemon
+systemctl enable systemsetup.service # enable systemsetup
+systemctl enable nix.automount # enable automount for /var/nix-store to /nix
