befd94d2e65c405cda4dd47a2ac665cc47a44583
certifiedfoolio <mastersvanity@gmail.com>
2024-12-31 06:58:51 -0800

    feat: enable mounts in / temporarily

--- scripts/quirks.sh
+++ scripts/quirks.sh
@@ -8,6 +8,7 @@ ln -s /usr/bin/systemctl /usr/bin/cserv
 
 # systemsetup and apx
 cp /ctx/system/applications/sysupdate.desktop /usr/share/applications/
+setenforce 1
 rm /usr/share/apx/stacks/vanilla.yaml \
     /usr/share/apx/stacks/vanilla-dev.yaml \
     /usr/share/apx/stacks/opensuse-leap-15.6.yaml
@@ -15,7 +16,17 @@ rm /usr/share/apx/stacks/vanilla.yaml \
 # copy system files
 cp -r /ctx/system/firstsetup /etc/
 cp -r /ctx/services/* /etc/systemd/system
-mv /nix /etc/nix-store
+mv /nix /etc/sysnix
+
+# change SELinux policy for Nix
+semanage fcontext -a -t etc_t '/etc/sysnix/store/[^/]+/etc(/.*)?'
+semanage fcontext -a -t lib_t '/etc/sysnix/store/[^/]+/lib(/.*)?'
+semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?'
+semanage fcontext -a -t man_t '/etc/sysnix/store/[^/]+/man(/.*)?'
+semanage fcontext -a -t bin_t '/etc/sysnix/store/[^/]+/s?bin(/.*)?'
+semanage fcontext -a -t usr_t '/etc/sysnix/store/[^/]+/share(/.*)?'
+semanage fcontext -a -t var_run_t '/etc/sysnix/var/nix/daemon-socket(/.*)?'
+semanage fcontext -a -t usr_t '/etc/sysnix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
 
 # enable systemd services/mounts
 systemctl enable nix-daemon # enable nix daemon
