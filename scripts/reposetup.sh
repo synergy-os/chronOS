ad65f72c2f8200de4b2f4fb6d808623012c03484
certifiedfoolio <mastersvanity@gmail.com>
2024-12-30 22:54:09 -0800

    fix: typo

--- scripts/reposetup.sh
+++ scripts/reposetup.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 wget -O /etc/yum.repos.d/_copr_pvermeer-chromebook-linux-audio.repo \
-    https://copr.fedorainfracloud.org/coprs/pvermeer/chromebook-linux-audio/repo/fedora-"${FEDORA_MAJOR_VERSION}"/pvermeer-chromebook-linux-audio-fedora-"${FEDORA_MAJOR_VERSION}".repo
-wget -O /etc/yum.repos.d/petersen-nix-fedora-"${FEDORA_MAJOR_VERSION}".repo \
-    https://copr.fedorainfracloud.org/coprs/petersen/nix/repo/fedora-"${FEDORA_MAJOR_VERSION}"/petersen-nix-fedora-"${FEDORA_MAJOR_VERSION}".repo
+    https://copr.fedorainfracloud.org/coprs/pvermeer/chromebook-linux-audio/repo/fedora-41/pvermeer-chromebook-linux-audio-fedora-41.repo
+wget -O /etc/yum.repos.d/petersen-nix-fedora-41.repo \
+    https://copr.fedorainfracloud.org/coprs/petersen/nix/repo/fedora-41/petersen-nix-fedora-41.repo
 cp /ctx/scripts/localrepos/google-chrome.repo /etc/yum.repos.d
