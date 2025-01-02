c22bb9b935fbc780e7dcb17582b1bfeae93c4f1b
certifiedfoolio <mastersvanity@gmail.com>
2025-01-02 01:58:22 -0800

    fix: typo

--- scripts/reposetup.sh
+++ scripts/reposetup.sh
@@ -5,5 +5,5 @@ set -ouex pipefail
 wget -O /etc/yum.repos.d/_copr_pvermeer-chromebook-linux-audio.repo \
     https://copr.fedorainfracloud.org/coprs/pvermeer/chromebook-linux-audio/repo/fedora-41/pvermeer-chromebook-linux-audio-fedora-41.repo
 # wget -O /etc/yum.repos.d/_copr_petersen-nix-fedora-41.repo \
-    https://copr.fedorainfracloud.org/coprs/petersen/nix/repo/fedora-41/petersen-nix-fedora-41.repo
+#    https://copr.fedorainfracloud.org/coprs/petersen/nix/repo/fedora-41/petersen-nix-fedora-41.repo
 cp -r /ctx/scripts/localrepos/* /etc/yum.repos.d/
