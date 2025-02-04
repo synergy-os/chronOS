e2f734cfa4cff37f1e18387761049772fbbe5f9b
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-03 21:09:17 -0800

    Create kderc.sh

---
+++ system/compile/kderc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BUILD_DEPS="cmake gcc-c++ extra-cmake-modules kwin-devel kf6-kconfigwidgets-devel libepoxy-devel kf6-kcmutils-devel kf6-ki18n-devel qt6-qtbase-private-devel wayland-devel
+"
+
+# prep
+rpm-ostree install $BUILD_DEPS
+
+# compile
+git clone https://github.com/matinlotfali/KDE-Rounded-Corners
+cd KDE-Rounded-Corners
+mkdir build
+cd build
+cmake ..
+cmake --build . -j
+make install
+
+# cleanup
+rpm-ostree override remove $BUILD_DEPS
