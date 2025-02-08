0108c5751d6aa8bb9424fd1050d7a8047814a51e
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-07 16:05:29 -0800

    fix: fixes

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # install pkgs
 rpm-ostree install gstreamer1-plugin-openh264 \
-	brave-browser-nightly \
+	/tmp/brave-browser-1.75.175-1.x86_64.rpm \
 	kweather \
 	kamoso \
 	kcalc \
