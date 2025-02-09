45051dd85a1fad9a021c392a86d9f4a2d374ff98
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-08 17:32:35 -0800

    fix: fixes

--- scripts/quirks.sh
+++ scripts/quirks.sh
@@ -37,4 +37,3 @@ systemctl enable flatpak-user-setup.service # enable flatpak user setup
 systemctl enable remove-system-flathub.service # enable system flathub removal
 systemctl enable remove-system-flatpaks.service # enable system flatpak removal
 systemctl enable systemsetup.service # enable system setup service
-systemctl enable config-falseopt.service # enable falseopt config 
