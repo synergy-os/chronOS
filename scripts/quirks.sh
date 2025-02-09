37df3a293399ae9f775651169d1fd83bc1ff08ea
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-09 08:52:26 -0800

    fix: file exec permissions

--- scripts/quirks.sh
+++ scripts/quirks.sh
@@ -7,6 +7,8 @@ ln -s /usr/bin/rpm-ostree /usr/bin/chruln
 ln -s /usr/bin/systemctl /usr/bin/cserv
 cp /ctx/system/bin/sudo /usr/bin/pkexec
 cp /ctx/system/bin/sudo /usr/bin/sudo
+chmod +x /usr/bin/sudo
+chmod +x /usr/bin/pkexec
 
 # systemsetup and apx
 cp /ctx/system/applications/sysupdate.desktop /usr/share/applications/
