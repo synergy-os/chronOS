7c0e1f1c4210ebdc4cfe61096e7edd1d7a67a185
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-11 20:08:42 +0000

    chore: remove solopaha/astal due to outdated

--- scripts/main/reposetup.sh
+++ scripts/main/reposetup.sh
@@ -9,7 +9,6 @@ dnf5 -y copr enable solopasha/hyprland
 dnf5 -y copr enable pvermeer/chromebook-linux-audio
 dnf5 -y copr enable bgstack15/AfterMozilla
 dnf5 -y copr enable pabrahamsson/hyprpanel
-dnf5 -y copr enable solopasha/astal
 dnf5 -y copr enable petersen/nix
 
 # cleanup
