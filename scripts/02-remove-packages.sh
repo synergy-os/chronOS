6aa7b16cb31dd910ff94558be63a1a61c5ff559c
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-16 14:35:00 -0800

    feat: reabse to base image

--- scripts/02-remove-packages.sh
+++ scripts/02-remove-packages.sh
@@ -5,22 +5,10 @@ set -ouex pipefail
 
 # remove pkgs from base image
 dnf5 -y remove htop \
-	plasma-desktop \
 	nvtop \
  sudo \
  	firefox-langpacks \
 	firefox \
 	fedora-logos \
 	toolbox \
-	sddm \
-	dolphin \
-	systemsettings \
-	kate \
-	kwrite \
-	filelight \
-	kde-partitionmanager \
-	kcharselect \
-	kwallet \
-	konsole \
-	krfb \
-	kdebugsettings
+	sddm
