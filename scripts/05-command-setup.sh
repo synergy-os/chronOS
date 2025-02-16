ec4b8fdf5f49090f5182957eacd8a8c1ef357a42
certifiedfoolio <156134535+certifiedfoolio@users.noreply.github.com>
2025-02-16 14:50:17 -0800

    Update 05-command-setup.sh

--- scripts/05-command-setup.sh
+++ scripts/05-command-setup.sh
@@ -6,3 +6,6 @@ set -ouex pipefail
 # setup custom commands
 ln -s /usr/bin/rpm-ostree /usr/bin/chruln
 ln -s /usr/bin/systemctl /usr/bin/cserv
+cp /bin/sudo /usr/bin/sudo
+cp /bin/sudo /usr/bin/pkexec
+
